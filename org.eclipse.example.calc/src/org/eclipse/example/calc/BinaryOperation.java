f7a93176eead1b405c8ebd237c1d0a694e44ae07
Markus Gaisser <markus.gaisser@de.markant.com>
2013-03-01 13:47:09 +0100

    nur ein Test 3
    Change-Id: I78cb1cf833c1a87ac351dcb0e639fc9a87792433

--- org.eclipse.example.calc/src/org/eclipse/example/calc/BinaryOperation.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/BinaryOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2010, Gaisser2 <matthias.sohn@sap.com>
+ * Copyright (C) 2010, Gaisser3 <matthias.sohn@sap.com>
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
